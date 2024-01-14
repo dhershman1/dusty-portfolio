a2ee7a98568a1c987f891ae777e16c8a005354fa
dhershman1 <dustinh17@gmail.com>
2024-01-14 00:01:47 -0500

    Lots of work on navigation and showing data

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,11 @@
 const typeText = document.getElementById('typeText')
 const textToType = 'Full Stack Software Engineer'
+const allowedHashes = new Set(['about', 'portfolio', 'skills'])
+const sections = {
+  about: document.getElementById('about'),
+  portfolio: document.getElementById('portfolio'),
+  skills: document.getElementById('skills')
+}
 let isAdding = true
 let idx = 1
 
@@ -28,4 +34,42 @@ function typeAnimation () {
   }, 120)
 }
 
-typeAnimation()
+function getHash (url) {
+  const [, hash] = url.split('#')
+
+  return hash
+}
+
+/**
+ * A function that shows and hides content based on the current hash string
+ * @param {String} hash A hash string we want to show the content of
+ */
+function showContent (hash) {
+  let useHash = hash
+
+  if (!allowedHashes.has(hash)) {
+    useHash = 'about'
+  }
+
+  sections[useHash].classList.remove('hide')
+
+  Object.keys(sections).forEach(k => {
+    if (k !== useHash) {
+      sections[k].classList.add('hide')
+    }
+  })
+
+}
+
+window.onload = () => {
+  typeAnimation()
+  addEventListener('hashchange', event => {
+    showContent(getHash(event.newURL))
+  })
+
+  if (!window.location.hash) {
+    window.location.hash = 'about'
+  }
+
+  showContent(window.location.hash.substring(1))
+}
